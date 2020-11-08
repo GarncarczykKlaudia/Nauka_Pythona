8f38c39a6e57378f30f827344752788e0522828c
GarncarczykKlaudia <GarncarczykKlaudia@users.noreply.github.com>
2020-11-08 13:19:04 +0100

    zad0,1,3

---
+++ prog0.py
@@ -0,0 +1,14 @@
+imie = 'Klaudia'
+iloscksiazek=10
+srednia=4.5
+
+print("Hello "+imie)
+print("Ma " + str(iloscksiazek))
+print("srednia: " +str(srednia))
+
+
+samolot = 'beoing'
+zamowienie = '10'
+zamowienie2 = '100'
+
+print("zeszlorocznezamowenie na"+samolot+"="+zamowienie2+"tegooczne:"+zamowienie)
---
+++ prog3.py
@@ -0,0 +1,20 @@
+marka="Kia"
+ilosc_drzwi=5
+pojemnosc=1.3
+
+
+marka_up = marka.upper()
+
+print ("Samochod" + marka  + "ma" + str(ilosc_drzwi) + " drzwi")
+print (marka_up)
+print("pojemnosc po zmianach: " + str(pojemnosc*2))
+
+
+marka_low=marka.lower()
+print(marka_low)
+
+A="Marka a Klaudia"
+
+x=A.replace("Klaudia", "Kia")
+
+print (x)
